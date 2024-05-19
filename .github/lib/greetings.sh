79d19eb53541e15db40c2d4cb6e581fd7ebc32a5
Rahul Gupta <rahulgupta@CKMAC71.local>
2024-05-19 23:42:19 +0530

    Updated scripts

---
+++ .github/lib/greetings.sh
@@ -0,0 +1,13 @@
+echo "Hello, Thanks for running this workflow"
+
+echo "BRANCH_NAME: $BRANCH_NAME"
+
+echo "ENV_VAR: $ENV_VAR"
+
+echo "ENV_SECRET: $ENV_SECRET"
+
+echo "NAME: $NAME"
+
+echo "AGE: $AGE"
+
+echo "SALARY: $SALARY"
