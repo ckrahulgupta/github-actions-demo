bb41de31fc3ef80596b2cc97e0fd8771bb48a00e
Rahul Gupta <rahul.gupta@cloudkaptan.com>
2025-04-07 23:19:21 +0530

    updated

--- .github/lib/retrieve-components.py
+++ .github/lib/retrieve-components.py
@@ -8,8 +8,8 @@ AUTH_URL 			= os.environ['AUTH_URL']
 API_VERSION 		= os.environ['API_VERSION']
 # GIT_USER_EMAIL 		= os.environ['GIT_USER_EMAIL']
 # GIT_USER_NAME 		= os.environ['GIT_USER_NAME']
-BASE_BRANCH 		= os.environ['BASE_BRANCH']
-WORKSPACE 			= os.environ['GITHUB_WORKSPACE']
+# BASE_BRANCH 		= os.environ['BASE_BRANCH']
+# WORKSPACE 			= os.environ['GITHUB_WORKSPACE']
 MAX_RETRIES			= 10
 
 os.system(f'sfdx project retrieve start --target-org {ORG_ALIAS} --manifest manifest/package.xml --api-version {API_VERSION}')
