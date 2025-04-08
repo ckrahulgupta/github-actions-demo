18f6edae886b6e6fc50a4692cacac88db1849dfe
Rahul Gupta <rahul.gupta@cloudkaptan.com>
2025-04-08 14:31:05 +0530

    updated

--- .github/lib/retrieve-components.py
+++ .github/lib/retrieve-components.py
@@ -1,4 +1,4 @@
-import os
+import os, subprocess
 
 print('Inside retrieve-components.py ...')
 
@@ -12,6 +12,17 @@ API_VERSION 		= os.environ['API_VERSION']
 # WORKSPACE 			= os.environ['GITHUB_WORKSPACE']
 MAX_RETRIES			= 10
 
-os.system(f'sfdx project retrieve start --target-org {ORG_ALIAS} --manifest manifest/package.xml --api-version {API_VERSION} > /dev/null')
+# retrieve all the components in the branch, based on the package.xml configured
+result = subprocess.run(
+            ['sfdx', 'project', 'retrieve', 'start', '--target-org', ORG_ALIAS, '--manifest', 'manifest/package.xml', '--api-version', API_VERSION], 
+            capture_output=True, 
+            text=True
+        )
+
+if result.returncode == 0:
+    print('******* Successfully retrieved all the components *******')
+else:
+    print('******* Failed to retrieve all the components *******')
+
 
 os.system('ls -alRf force-app/main/default')
