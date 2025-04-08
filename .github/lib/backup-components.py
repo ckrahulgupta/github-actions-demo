4ffec349aa60ed20b07c94611385e8455cbdd398
Rahul Gupta <rahul.gupta@cloudkaptan.com>
2025-04-08 15:57:07 +0530

    updated

--- .github/lib/backup-components.py
+++ .github/lib/backup-components.py
@@ -1,1 +1,32 @@
-print('Inside backup-components.py ...')
+import os, subprocess
+
+# constants
+ORG_ALIAS 			= os.environ['ORG_ALIAS']
+API_VERSION 		= os.environ['API_VERSION']
+GIT_USER_EMAIL 		= os.environ['GIT_USER_EMAIL']
+GIT_USER_NAME 		= os.environ['GIT_USER_NAME']
+BASE_BRANCH 		= os.environ['BASE_BRANCH']
+WORKSPACE 			= os.environ['GITHUB_WORKSPACE']
+
+# mark the directory as safe
+os.system(f'git config --global --add safe.directory {WORKSPACE}')
+
+# set the git username and email
+os.system(f'git config --global user.email {GIT_USER_EMAIL}')
+os.system(f'git config --global user.name {GIT_USER_NAME}')
+
+# stage and commit the changes
+os.system('git add .')
+os.system(f'git commit -m "backup: sync all components from {BASE_BRANCH} org to {BASE_BRANCH} backup branch"')
+
+# push the changes to remote
+result = subprocess.run(
+			['git', 'push', 'origin', BASE_BRANCH], 
+			capture_output=True, 
+			text=True
+		)
+
+if result.returncode == 0:
+    print('******* Successfully pushed to remote *******')
+else:
+    print('******* Failed to push to remote *******')
--- .github/lib/retrieve-components.py
+++ .github/lib/retrieve-components.py
@@ -1,16 +1,7 @@
 import os, subprocess
 
-print('Inside retrieve-components.py ...')
-
 ORG_ALIAS 			= os.environ['ORG_ALIAS']
-# ORG_MY_DOMAIN_URL	= os.environ['ORG_MY_DOMAIN_URL']
-AUTH_URL 			= os.environ['AUTH_URL']
 API_VERSION 		= os.environ['API_VERSION']
-# GIT_USER_EMAIL 		= os.environ['GIT_USER_EMAIL']
-# GIT_USER_NAME 		= os.environ['GIT_USER_NAME']
-# BASE_BRANCH 		= os.environ['BASE_BRANCH']
-# WORKSPACE 			= os.environ['GITHUB_WORKSPACE']
-MAX_RETRIES			= 10
 
 # retrieve all the components in the branch, based on the package.xml configured
 result = subprocess.run(
@@ -23,6 +14,3 @@ if result.returncode == 0:
     print('******* Successfully retrieved all the components *******')
 else:
     print('******* Failed to retrieve all the components *******')
-
-
-os.system('ls -alRf force-app/main/default')
